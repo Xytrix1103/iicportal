013f58a35af4762f342a5b8a737549d6a7ef186e
Ho Shu Yi <syho031001@gmail.com>
2023-09-11 11:54:55 +0800

    update login

--- app/src/main/java/com/iicportal/activity/LoginActivity.java
+++ app/src/main/java/com/iicportal/activity/LoginActivity.java
@@ -2,7 +2,7 @@ package com.iicportal.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -10,6 +10,8 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.iicportal.R;
 
 public class LoginActivity extends AppCompatActivity {
@@ -17,60 +19,79 @@ public class LoginActivity extends AppCompatActivity {
 
     Button loginButton;
 
-    TextView forgotPasswordText, registerText;
+    TextView forgotPasswordButton, registerButton;
+
+    FirebaseAuth mAuth;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
+
         usernameEdit = findViewById(R.id.username);
         passwordEdit = findViewById(R.id.password);
         loginButton = findViewById(R.id.loginBtn);
-        forgotPasswordText = findViewById(R.id.forgotPassword);
+        forgotPasswordButton = findViewById(R.id.forgotPasswordBtn);
+        registerButton = findViewById(R.id.registerBtn);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String username = usernameEdit.getText().toString();
-                String password = passwordEdit.getText().toString();
+        loginButton.setOnClickListener(v -> {
+            String username = usernameEdit.getText().toString();
+            String password = passwordEdit.getText().toString();
 
-                if (username.isEmpty()) {
-                    usernameEdit.setError("Username is required");
-                    usernameEdit.requestFocus();
-                    return;
-                }
-
-                if (password.isEmpty()) {
-                    passwordEdit.setError("Password is required");
-                    passwordEdit.requestFocus();
-                    return;
-                }
-
-                if (username.equals("admin") && password.equals("admin")) {
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                } else {
-                    Toast.makeText(LoginActivity.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
-                }
+            if (username.isEmpty()) {
+                usernameEdit.setError("Username is required");
+                usernameEdit.requestFocus();
+                return;
             }
+
+            if (password.isEmpty()) {
+                passwordEdit.setError("Password is required");
+                passwordEdit.requestFocus();
+                return;
+            }
+
+            Log.d("LoginActivity", "Username: " + username + ", Password: " + password);
+
+            mAuth.signInWithEmailAndPassword(username, password)
+                    .addOnCompleteListener(this, task -> {
+                        Log.d("LoginActivity", "signInWithEmail:onComplete:" + task.isSuccessful());
+                        if (task.isSuccessful()) {
+                            Log.d("LoginActivity", "signInWithEmail:success");
+                            user = mAuth.getCurrentUser();
+                            reload();
+                        } else {
+                            Log.w("LoginActivity", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
         });
 
-//        registerText.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-//                startActivity(intent);
-//                finish();
-//            }
-//        });
-
-        forgotPasswordText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(LoginActivity.this, "Forgot password", Toast.LENGTH_SHORT).show();
-            }
+        registerButton.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
+            finish();
         });
+
+        forgotPasswordButton.setOnClickListener(v -> Toast.makeText(LoginActivity.this, "Forgot password", Toast.LENGTH_SHORT).show());
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            reload();
+        }
+    }
+
+    private void reload() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
