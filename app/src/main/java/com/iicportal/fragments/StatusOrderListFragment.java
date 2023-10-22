2472b328a5758229d3dd8c473837f8e4282b45d8
Ho Shu Yi <syho031001@gmail.com>
2023-10-22 17:27:28 +0800

    added admin view for order details

--- app/src/main/java/com/iicportal/fragments/StatusOrderListFragment.java
+++ app/src/main/java/com/iicportal/fragments/StatusOrderListFragment.java
@@ -11,11 +11,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 import com.iicportal.R;
 import com.iicportal.activity.MainActivity;
 import com.iicportal.adaptor.StatusOrderListAdaptor;
@@ -30,7 +28,7 @@ public class StatusOrderListFragment extends Fragment {
     StatusOrderListAdaptor statusOrderListAdaptor;
     int status;
 
-    Query query = null;
+    Query query;
 
     public StatusOrderListFragment() {
         super(R.layout.status_order_list_fragment);
@@ -39,7 +37,7 @@ public class StatusOrderListFragment extends Fragment {
         mAuth = MainActivity.mAuth;
         user = MainActivity.user;
         status = 0;
-        query = ordersRef.orderByChild("uid").equalTo(user.getUid()).getRef().orderByChild("completedTimestamp").equalTo(null).getRef().orderByChild("readyTimestamp").equalTo(null);
+        query = ordersRef.orderByChild("completed").equalTo(false).getRef().orderByChild("ready").equalTo(false);
     }
 
     public void setStatus(int status) {
@@ -48,37 +46,18 @@ public class StatusOrderListFragment extends Fragment {
 
         switch(status) {
             case 0:
-                Log.d("Query", "query = ordersRef.orderByChild(\"uid\").equalTo(user.getUid()).getRef().orderByChild(\"completedTimestamp\").equalTo(null).getRef().orderByChild(\"readyTimestamp\").equalTo(null).getRef();");
-                query = ordersRef.orderByChild("uid").equalTo(user.getUid()).getRef().orderByChild("completed").equalTo(false).getRef().orderByChild("ready").equalTo(false);
+                query = ordersRef.orderByChild("completed").equalTo(false).getRef().orderByChild("ready").equalTo(false);
                 break;
             case 1:
-                Log.d("Query", "query = ordersRef.orderByChild(\"uid\").equalTo(user.getUid()).getRef().orderByChild(\"completedTimestamp\").equalTo(null).getRef().orderByChild(\"readyTimestamp\").startAt(0.0);");
-                query = ordersRef.orderByChild("uid").equalTo(user.getUid()).getRef().orderByChild("ready").equalTo(true).getRef().orderByChild("completed").equalTo(false);
+                query = ordersRef.orderByChild("ready").equalTo(true).getRef().orderByChild("completed").equalTo(false);
                 break;
             case 2:
-                Log.d("Query", "query = ordersRef.orderByChild(\"uid\").equalTo(user.getUid()).getRef().orderByChild(\"completedTimestamp\").startAt(0.0);");
-                query = ordersRef.orderByChild("uid").equalTo(user.getUid()).getRef().orderByChild("completed").equalTo(true);
+                query = ordersRef.orderByChild("completed").equalTo(true);
                 break;
             default:
                 Log.d("Status", "Default");
                 break;
         }
-
-        query.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()) {
-                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                        Log.d("Status", "timestamps: " + dataSnapshot.getValue(Order.class).getReadyTimestamp() + " completed: " + dataSnapshot.getValue(Order.class).getCompletedTimestamp());
-                    }
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull com.google.firebase.database.DatabaseError error) {
-
-            }
-        });
 
         statusOrderListAdaptor.stopListening();
 
@@ -90,6 +69,8 @@ public class StatusOrderListFragment extends Fragment {
         statusOrderListAdaptor.updateOptions(options);
 
         statusOrderListAdaptor.startListening();
+
+        statusOrderListAdaptor.notifyDataSetChanged();
     }
 
     @Override
